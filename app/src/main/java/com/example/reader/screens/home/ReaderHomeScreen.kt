17aab73f15ef16fa8cac7afe3101c1e90b84e9ad
ardaaltinorsardaaltinors@gmail.com <ardaaltinors@gmail.com>
2024-01-26 19:44:04 +0300

    SplashScreen animation

---
+++ app/src/main/java/com/example/reader/screens/home/ReaderHomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.reader.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun Home(navController: NavController) {
+    
+}
---
+++ app/src/main/java/com/example/reader/screens/stats/ReaderStatsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.reader.screens.stats
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReaderStatsScreen() {
+    
+}
